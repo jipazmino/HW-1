43fb26f0da7df86e1558ac8763d8852e445e37c4
jipazmino <jpazminomino@cca.edu>
2017-01-23 01:22:00 -0800

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,20 @@
+Level 5
+
+function rightTwoTimes() {
+  right();
+	right();  
+}
+function BlueorRed() {
+  if (getColor() == "blue") {
+  down();
+} else {
+  up();
+}}
+rightTwoTimes();
+BlueorRed();
+rightTwoTimes();
+BlueorRed();
+rightTwoTimes();
+BlueorRed();
+rightTwoTimes();
+right();
