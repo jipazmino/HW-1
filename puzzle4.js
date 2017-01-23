ee14a2cfb91050dfec0ad6f6a2aabd5db5e8fa96
jipazmino <jpazminomino@cca.edu>
2017-01-23 01:21:35 -0800

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,18 @@
+Level 4
+
+function GetColorAndDrop() {
+	var c = getColor();
+  down();
+	setColor(c);
+	right();
+	right();
+}
+down();
+down();
+down();
+down();
+right();
+GetColorAndDrop();
+up();
+right();
+GetColorAndDrop();
