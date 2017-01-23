5f401bcc35ef71e9c636cf4ab5995ffb66cc3aa9
jipazmino <jpazminomino@cca.edu>
2017-01-23 01:18:49 -0800

    Create puzzle1.js

---
+++ puzzle1.js
@@ -0,0 +1,21 @@
+Level 1
+
+function downSevenTimes() {
+	down();
+	down();
+	down();
+	down();
+	down();
+	down();
+	down();
+}
+right();
+downSevenTimes();
+right();
+right();
+right();
+right();
+up();
+up();
+right();
+right();
