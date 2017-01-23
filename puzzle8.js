0b24efdf021049b6e5a0c9e694baf7457b0029de
jipazmino <jpazminomino@cca.edu>
2017-01-23 01:23:46 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,34 @@
+function rightTwo() {
+  right();
+  right();
+}
+var steps = 0;
+while (steps < 8) {
+  down();
+  steps = steps + 1;
+}
+rightTwo();
+var steps = -7;
+while (steps < 0) {
+  up();
+  steps = steps + 1;
+}
+rightTwo();
+var steps = 0;
+while (steps < 7) {
+  down();
+  steps = steps + 1;
+}
+rightTwo();
+var steps = -7;
+while (steps < 0) {
+  up();
+  steps = steps + 1;
+}
+rightTwo();
+var steps = 0;
+while (steps < 7) {
+  down();
+  steps = steps + 1;
+}
+right();
